df6b93344177ba1ca441ee7ac54bb18e68562e4b
sahabulislamsifat <sahabulislamsifat@gmail.com>
2024-10-18 18:33:34 +0600

    props distructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Developer></Developer>
       <Person></Person>
+      <Device name="Laptop" price="55000"></Device>
+      <Device name="Mobile" price="17000"></Device>
+      <Device name="Watch" price="3000"></Device>
+      <Student grade="7" section="A"></Student>
+      <Student grade="14" section="B"></Student>
       <Student></Student>
     </>
+  );
+}
+
+function Device(props) {
+  return (
+    <h2>
+      This Device: {props.name} Price: {props.price}
+    </h2>
   );
 }
 
@@ -25,11 +36,32 @@ function Person() {
   );
 }
 
-function Student() {
+// const { grade, section } = { grade: "7", section: "A" };
+
+function Student({ grade = 0, section = "N/A" }) {
+  // console.log(grade, section);
   return (
-    <div>
-      <h4>Name : Sahabul Islam Sifat</h4>
+    <div className="student">
+      {/* <h4>Name : Sahabul Islam Sifat</h4> */}
+      <h4>This is a Ideal Student</h4>
+      <p>Grade : {grade}</p>
+      <p>Section : {section}</p>
       <p>Age : 22</p>
+    </div>
+  );
+}
+
+function Developer() {
+  const developerStyle = {
+    margin: "20px",
+    padding: "20px",
+    border: "2px solid purple",
+    borderRadius: "20px",
+  };
+  return (
+    <div style={developerStyle}>
+      <h4>Chapri Developer by React</h4>
+      <p>Coding :</p>
     </div>
   );
 }
